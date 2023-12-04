f216357df32301c6e1e995afe04c7b1c44c53d81
Valunchanut <125695721+valunchanut@users.noreply.github.com>
2023-12-03 23:46:11 -0500

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,26 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import EmployeeList from './components/EmployeeList';
 import Navigation from './components/Navigation';
+import './App.css';
 
 function App() {
     return (
         <Router>
-            <Navigation />
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/employees" component={EmployeeList} />
-            </Switch>
+            <div className="App">
+                <header className="App-header">
+                    <Navigation />
+                </header>
+                <main className="App-content">
+                    <Switch>
+                        <Route path="/login" component={Login} />
+                        <Route path="/signup" component={Signup} />
+                        <Route path="/employees" component={EmployeeList} />
+                    </Switch>
+                </main>
+                <footer className="App-footer">
+                    Created by Valunchanut Simaroj
+                </footer>
+            </div>
         </Router>
     );
 }
